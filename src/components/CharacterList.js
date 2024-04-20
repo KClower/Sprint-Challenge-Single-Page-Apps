9b2434afb823a3452303c290b412f155f7cc84c1
KClower <kclower3@gmail.com>
2024-04-19 20:32:04 -0500

    finished MVP

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,63 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
+import CharacterCard from "./CharacterCard";
+import SearchForm from "./SearchForm";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([])
+  const [filteredCharacter, setFilteredCharacter] = useState([])
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
+    const getCharacters = () => {
+      axios
+       .get('https://rickandmortyapi.com/api/character')
+       .then(response => {
+        
+        setCharacters(response.data.results)
+       })
+       .catch(error => {
+        console.error("server error", error);
+       })
+    }
+   getCharacters();
   }, []);
+  console.log(characters)
+
+const filterCharacters = (searchTerm) => {
+console.log(searchTerm)
+const result = characters.filter(character => {
+  return character.name.toLowerCase().includes(searchTerm.toLowerCase())
+
+})
+setFilteredCharacter(result)
+}
+
+const characterArray = filteredCharacter.length === 0 ? characters : filteredCharacter
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+    <div>
+      <SearchForm searchHandler={filterCharacters}/>
+    <section className="character-list d-flex flex-wrap justify-content-evenly">
+    {characterArray.map(character => {
+  
+      return( 
+         <CharacterCard
+        id={character.id}
+        image={character.image}
+        name={character.name}
+        species={character.species}
+        status={character.status}
+        location={character.location}
+        origin={character.origin}
+         />
+       )
+  
+    })}
     </section>
+    </div>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,30 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
+export default function SearchForm(props) {
+
+ const {searchHandler}=props
+
+ const [searchTerm, setSearchTerm] = useState("")
  
-  return (
-    <section className="search-form">
-     // Add a search form here
+
+ const SubmitHandler = evt => {
+  evt.preventDefault();
+  searchHandler(searchTerm)
+  setSearchTerm("")
+ }
+
+const onChange = evt => {
+
+  console.log(evt.target.value)
+  setSearchTerm(evt.target.value)
+}
+
+ return (
+    <section className="search-form text-center">
+     <form onSubmit={SubmitHandler}>
+      <label className="me-2">Search Character</label>
+      <input onChange={onChange} value={searchTerm} type="text"/>
+     </form>
     </section>
   );
 }
