a35b8b06c619f24cdc7a0da0b36438b8f20cf3aa
KClower <kclower3@gmail.com>
2024-04-21 09:51:21 -0500

    added the locations page

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,49 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect  } from "react";
+import axios from "axios";
+import LocationCard from "./LocationCard";
 
-export default function LocationsList() {}
+
+
+export default function LocationsList() {
+
+    const [locations, setLocations] = useState([])
+
+
+ useEffect(() => {
+    const getLocations = () => {
+      axios
+       .get('https://rickandmortyapi.com/api/location')
+       .then(response => {
+        console.log(response.data.results)
+        setLocations(response.data.results)
+      })
+       .catch(error => {
+        console.error("server error", error);
+      })
+    }
+      getLocations();    
+    }, []);
+
+
+return (
+    <div>
+        <section className="location-list d-flex flex-wrap justify-content-evenly">
+    {locations.map(location => {
+        return(
+            <LocationCard 
+            id={location.id}
+            name={location.name}
+            type={location.type}
+            dimension={location.dimension}
+            residents={location.residents}
+            />
+        )
+    })}
+  
+  </section>
+    </div>
+)
+
+
+
+}
